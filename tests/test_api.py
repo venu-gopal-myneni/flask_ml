acbb2931133f6df0bdde18cf349dea1bc12b449c
venu <mailvenumyneni@gmail.com>
2023-06-16 13:33:47 +0530

    Test cases  for api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,25 +1,27 @@
-import pytest
-from api import app as crea
 
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Welcome to ML App" in response.data
 
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
+def test_tenants(client):
+    response = client.get("/tenants")
+    assert response.status_code == 200
 
 
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+def test_metadata(client):
+    response = client.get("/metadata")
+    assert response.status_code == 200
 
+def test_post_tenant(client,random_data):
+    name = random_data["name"]
+    email = random_data["email"]
+    response = client.post("/add_tenant", json=dict(tenantname= name,email=email ))
+    assert response.status_code == 200
 
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+def test_post_metadata(client,random_data):
+    filename = random_data["csv_file"]
+    eval = random_data["eval"]
+    tenant_id=random_data["id"]
+    response = client.post("/add_metadata", json=dict(filename= filename,eval=eval,tenant_id=tenant_id ))
+    assert response.status_code == 200
